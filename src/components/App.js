7769e591afa8f9266baac99d21b85eedafb9375d
Artyom Belsky <artyombel2003@gmail.com>
2022-11-24 23:31:43 +0300

    Some components folder added. React-Bootstrap button was drawn.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap'
+
+
+const App = () => {
+    return (
+        <div>
+            <Button variant="primary">Primary</Button>{' '}
+        </div>
+    )
+}
+
+export default App
